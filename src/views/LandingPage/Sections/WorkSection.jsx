4f8709bb055aca8035f9ee1e6f941278edeb7531
孫 葦傑 <C112113153@nkust.edu.tw>
2025-06-21 23:04:07 +0800

    fix file upload parsing and chart setup

--- src/views/LandingPage/Sections/WorkSection.jsx
+++ src/views/LandingPage/Sections/WorkSection.jsx
@@ -18,7 +18,7 @@ class WorkSection extends React.Component {
     return (
       <div className={classes.section}>
         <GridContainer justify="center">
-          <GridItem cs={12} sm={12} md={8}>
+          <GridItem xs={12} sm={12} md={8}>
             <h2 className={classes.title}>Work with us</h2>
             <h4 className={classes.description}>
               Divide details about your product or agency work into parts. Write
